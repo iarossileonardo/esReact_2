ec1e86bd392bbab24c57ab47b2f1503eaa553db7
lai2-24 <lai2-24@itismeucci.com>
2025-04-30 10:29:37 +0200

    finito

--- app/src/Square.js
+++ app/src/Square.js
@@ -1,9 +1,15 @@
 import './Square.css';
 import {useState} from 'react';
 
-export default function Square() {
+export default function Square({ value, onSquareClick}) {
 
-    const [value, setValue] = useState(null)
+    let colore;
 
-    return <button className="square">{value}</button>;
+    if (value === "X") {
+        colore = "red";
+    } else {
+        colore = "blue";
+    }
+
+    return <button className={`square ${colore}`}  onClick={onSquareClick}>{value}</button>;
 }
